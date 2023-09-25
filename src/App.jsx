8cd93954d5a8e0ce9588bc3c4d23ae98fc7c3e53
abuabdullah23 <abuobaidazayed@gmail.com>
2023-09-25 09:12:25 +0600

    Project setup with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h2>
+          Donation Campaign
+        </h2>
+      </div>
+    </>
+  )
+}
+
+export default App

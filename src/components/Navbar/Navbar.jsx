95cf393309e1d053d286dbe242207958ce103bc0
abuabdullah23 <abuobaidazayed@gmail.com>
2023-09-26 22:22:42 +0600

    Navbar updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,12 +11,15 @@ const Navbar = () => {
     return (
         <nav className='bg-white sticky top-0 z-10 drop-shadow-sm'>
             <Container>
-                <div onClick={() => setOpen(!open)} className='py-2 md:hidden'>
+                <div onClick={() => setOpen(!open)} className='py-2 md:hidden flex items-center gap-5'>
                     <span>{
                         open === true ?
                             <MdClose className="h-6 w-6" />
                             : <BsList className="h-6 w-6" />
                     }</span>
+                    <Link to={'/'} onClick={() => setOpen(!open)}>
+                        <img src={logo} className='h-[36px] md:h-[56px]' alt="logo" />
+                    </Link>
                 </div>
 
                 <div className={`bg-white rounded-md md:flex md:justify-between md:items-center absolute md:static duration-1000 mt-6 pl-10 pe-10 pb-3 md:mt-0 md:pt-3 w-full ${open ? 'top-4' : '-top-[384px]'}`}>
